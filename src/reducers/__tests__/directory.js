3f58af88e0f592e67b57dd2661db0d4a6df28383
Stephen Saunders <saunders.stephen@gmail.com>
2015-08-03 13:36:01 -0700

    rewrote reducers/directory in ES6, moved const declarations within each each test function, and clarified the describe block where appropriate

--- src/reducers/__tests__/directory.js
+++ src/reducers/__tests__/directory.js
@@ -1,94 +1,117 @@
 'use strict';
 
-var expect = require('expect');
+const expect = require('expect');
 
-var directory = require('../directory');
+const directory = require('../directory');
+const {
+  SAVE_FILE,
+  DELETE_FILE,
+  CHANGE_DIRECTORY,
+  REFRESH_DIRECTORY
+  } = require('../../constants');
 
 describe('#directory', function(){
-  var state, type, payload, action;
 
-  beforeEach(function(){
-    state = ['file1', 'file2'];
-    type = 'NOT_VALID_TYPE';
-    payload = {
-      listing: 'file3'
+  it('returns empty array when initial state is undefined and type does not match', function(done){
+    const initial = undefined;
+    const type = undefined;
+    const payload = {
+      listing: [
+        'file1',
+        'file2'
+      ]
     };
-    action = {
-      type: type,
-      payload: payload
-    };
-  });
-
-  afterEach(function(){
-    state = type = payload = action = null;
-  });
-
-  it('returns an empty array when state is undefined and type does not match', function(done){
-    state = undefined;
-    var response = directory(state, action);
-    expect(response instanceof Array).toBe(true);
-    expect(response.length).toBe(0);
-    done();
-  });
-
-  it('throws an error if action.payload is undefined', function(done){
-    action.payload = undefined;
-    action.type = 'SAVE_FILE';
-    expect(function(){
-      directory(state, action);
-    }).toThrow(/Cannot read property 'listing'/);
+    const state = directory(initial, { type: type, payload: payload });
+    expect(state).toEqual([]);
     done();
   });
 
   it('returns undefined if action.payload.listing is undefined', function(done){
-    action.payload.listing = undefined;
-    action.type = 'SAVE_FILE';
-    expect(directory(state, action)).toBeAn('undefined');
+    const initial = 'test';
+    const type = SAVE_FILE;
+    const payload = {};
+    const state = directory(initial, { type: type, payload: payload });
+    expect(state).toEqual(undefined);
     done();
   });
 
-  it('returns state when type is not matched', function(done){
-    expect(directory(state, action)).toBe(state);
-    done();
-  });
-
-  it.skip('throws an error if state is not an array', function(done){
-    state = 'string';
-    expect(function() {
-      directory(state, action);
-    }).toThrow(/Type Error: 'state' is not an Array/);
+  it('returns initial state when type is not matched', function(done){
+    const initial = 'test';
+    const type = undefined;
+    const payload = {};
+    const state = directory(initial, { type: type, payload: payload });
+    expect(state).toEqual(initial);
     done();
   });
 
   describe('returns action.payload.listing when type matches', function(){
     it('SAVE_FILE', function(done){
-      action.type = 'SAVE_FILE';
-      expect(directory(state, action)).toBe(action.payload.listing);
+      const initial = 'test';
+      const type = SAVE_FILE;
+      const payload = {
+        listing: [
+          'file1',
+          'file2'
+        ]
+      };
+      const state = directory(initial, { type: type, payload: payload });
+      expect(state).toEqual(payload.listing);
       done();
     });
 
     it('DELETE_FILE', function(done){
-      action.type = 'DELETE_FILE';
-      expect(directory(state, action)).toBe(action.payload.listing);
+      const initial = 'test';
+      const type = DELETE_FILE;
+      const payload = {
+        listing: [
+          'file1',
+          'file2'
+        ]
+      };
+      const state = directory(initial, { type: type, payload: payload });
+      expect(state).toEqual(payload.listing);
       done();
     });
 
     it('CHANGE_DIRECTORY', function(done){
-      action.type = 'CHANGE_DIRECTORY';
-      expect(directory(state, action)).toBe(action.payload.listing);
+      const initial = 'test';
+      const type = CHANGE_DIRECTORY;
+      const payload = {
+        listing: [
+          'file1',
+          'file2'
+        ]
+      };
+      const state = directory(initial, { type: type, payload: payload });
+      expect(state).toEqual(payload.listing);
       done();
     });
 
     it('REFRESH_DIRECTORY', function(done){
-      action.type = 'REFRESH_DIRECTORY';
-      expect(directory(state, action)).toBe(action.payload.listing);
+      const initial = 'test';
+      const type = REFRESH_DIRECTORY;
+      const payload = {
+        listing: [
+          'file1',
+          'file2'
+        ]
+      };
+      const state = directory(initial, { type: type, payload: payload });
+      expect(state).toEqual(payload.listing);
       done();
     });
 
-    it('even if state is undefined', function(done){
-      action.type = 'REFRESH_DIRECTORY';
-      state = undefined;
-      expect(directory(state, action)).toBe(action.payload.listing);
+    it('even if initial state is undefined', function(done){
+      const initial = undefined;
+      const type = SAVE_FILE;
+      const payload = {
+        listing: [
+          'file1',
+          'file2'
+        ]
+      };
+      const state = directory(initial, { type: type, payload: payload });
+      expect(state).toEqual(payload.listing);
       done();
     });
   });
