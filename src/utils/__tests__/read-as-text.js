4d15fd3d650221596080bb1fadc472e021539f16
Stephen Saunders <saunders.stephen@gmail.com>
2015-08-14 00:02:05 -0700

    added test for case that FileReader.readAsText cannot process the input

--- src/utils/__tests__/read-as-text.js
+++ src/utils/__tests__/read-as-text.js
@@ -14,11 +14,13 @@ describe('readAsText Util', function(){
       .then(() => done(), done);
   });
 
-  it.skip('returns a rejected promise when file is not read', function(done){
-    const file = new File('not file', Date.now());
+  it('throws an error if file is not read', function(done){
+    const file = new Array(['not file', Date.now()]);
     readAsText(file)
-      .then(function(text){
-        expect(text).toEqual('world');
+      .then(function(res){
+        throw Error('Should have thrown an error', res);
+      }, function(err){
+        expect(err.message).toInclude(`Failed to execute 'readAsText'`);
       })
       .then(() => done(), done);
   });
